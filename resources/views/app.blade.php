c2e16ddd0a384a21c5601834a09f78c68e905231
Nelson Orina <orinamecha2@gmail.com>
2025-11-08 21:24:01 +0300

    Migration from blade templates to react components

---
+++ resources/views/app.blade.php
@@ -0,0 +1,13 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    @viteReactRefresh
+    @vite(['resources/js/app.jsx', 'resources/css/app.css'])
+    @inertiaHead
+</head>
+<body>
+    @inertia 
+</body>
+</html> 
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,13 @@
 <?php
 
 use App\Models\Post;
+use Inertia\Inertia;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\UserController;
 
 Route::get('/', function () {
-    //These are used to list all posts of the logged-in user
-    $posts =  [];
-    if(auth()->check()){
-        $posts = auth()->user()->posts()->get();
-    }
-    // $posts = Post::where('user_id',auth()->id())->get();
-
-    //$posts = Post::all(); // This will fetch all posts from the database, regardless of the user
-
-    return view('home', ['posts' => $posts]);
+   return Inertia::render('Home', ['name' => 'Nelson']);
 });
 
 Route::post('/register', [UserController::class, 'register']);
